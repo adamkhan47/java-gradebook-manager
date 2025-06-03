9daff5ae564630233f5407cdd726ce4c123a3c77
Adam Khan <adammkhan47@gmail.com>
2025-06-03 17:27:18 -0500

    make some new files.

--- Gradebook.java
+++ Gradebook.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class Gradebook () {
-    
+    private int finalGrade;
+    private String courseName;
+    private ArrayList grades;
+
+    public GradeBook() {
+        finalGrade = 0;
+        courseName = "";
+    }
+    public GradeBook(String course, ArrayList gradesList) {
+        finalGrade = 0;
+        courseName = course;
+        grades = gradesList;
+    }
 }
---
+++ GradebookRunner.java
@@ -0,0 +1,6 @@
+public class GradebookRunner {
+    public static void main(String[] args) {
+        Gradebook test = new Gradebook();
+        System.out.println("test");
+    }
+}
