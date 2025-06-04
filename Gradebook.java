89dbc06a651150fb93443564f3b72f1b5dcbc512
Adam Khan <adammkhan47@gmail.com>
2025-06-03 20:03:38 -0500

    fix it to make it actually compile lol

--- Gradebook.java
+++ Gradebook.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
 
-public class Gradebook () {
+public class Gradebook {
     private int finalGrade;
     private String courseName;
-    private ArrayList grades;
+    private ArrayList<Integer> grades;
 
-    public GradeBook() {
+    public Gradebook() {
         finalGrade = 0;
         courseName = "";
+        grades = new ArrayList<>();
     }
-    public GradeBook(String course, ArrayList gradesList) {
+    public Gradebook(String course, ArrayList<Integer> gradesList) {
         finalGrade = 0;
         courseName = course;
         grades = gradesList;
