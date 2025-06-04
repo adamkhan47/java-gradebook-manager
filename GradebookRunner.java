2f04377b181f60bf5f9385b6a846655725b7a9d4
Adam Khan <adammkhan47@gmail.com>
2025-06-03 20:21:14 -0500

    First stage to refactoring project name.

--- GradebookRunner.java
+++ GradebookRunner.java
@@ -1,6 +1,6 @@
 public class GradebookRunner {
     public static void main(String[] args) {
         Gradebook test = new Gradebook();
-        System.out.println("test");
+        System.out.println("Welcome to the What if grade calculator for java.");
     }
 }
