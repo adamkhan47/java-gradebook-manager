21385f24454e5a9d5be3a42602708203257dbf69
Adam Khan <adammkhan47@gmail.com>
2025-06-03 21:42:20 -0500

    Most logic done, now bug fixing

--- WhatIf.java
+++ WhatIf.java
@@ -1,18 +1,35 @@
 import java.util.Arrays;
 
 public class WhatIf {
-    private int finalGrade;
+    private double finalGrade;
     private String courseName;
-    private int[] grades;
+    private double[] summativegrades;
+    private double[] formativegrades;
 
     public WhatIf() {
         finalGrade = 0;
         courseName = "";
     }
-    public WhatIf(String course, int[] gradesList) {
+    public WhatIf(String course, double[] fgradesList, double[] sgradesList) {
         finalGrade = 0;
         courseName = course;
-        grades = gradesList;
+        summativegrades = fgradesList;
+        formativegrades = sgradesList;
+    }
+
+    public void calcAverage() {
+        double formtemp = 0;
+        for (int i = 0; i<formativegrades.length; i++) {
+            formtemp = formtemp + formativegrades[i];
+        }
+        double sumtemp = 0;
+        for (int i = 0; i<summativegrades.length; i++) {
+            sumtemp = sumtemp + summativegrades[i];
+        } 
+        finalGrade = (formtemp * 0.3) + (sumtemp * 0.7);
+    }
+    public String toString() {
+        return ("The final grade after averaging everything in " + courseName + " is a " + finalGrade);
     }
 
 }
--- WhatIfRunner.java
+++ WhatIfRunner.java
@@ -5,24 +5,42 @@ public class WhatIfRunner {
         System.out.println("Welcome to the What if grade calculator for java.");
         System.out.println("This java program will calculate an average grade based on the formative and summative grades you provide.");
         System.out.println("You can enter grades, and the program will compute the average grade for you.");
-        System.out.println("You can also enter a course name to keep track of your grades for that course.");
+        System.out.println("Enter a course name to keep track of your grades for that course.");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the course name: ");
         String courseName = scanner.nextLine();
 
         // make it so that we can put in a integer and it converts to double k
         // actually i think java does that automatically
-        System.out.println("Now enter your summative grades by using the format in the example below:");
+        System.out.println("Now enter your *summative* grades by using the format in the example below:");
         System.out.println("Example: 100, 99, 39, 50, 100");
         System.out.println("You can also just put in \"Z\" or \"X\" to signify if its a late or exempt assignment.");
-        String arrayString = scanner.nextLine();
+        String arraySummativeString = scanner.nextLine();
+        System.out.println("Now enter your *formative* grades by using the format in the example below:");
+        System.out.println("Example: 100, 99, 39, 50, 100");
+        System.out.println("You can also just put in \"Z\" or \"X\" to signify if its a late or exempt assignment.");
+        String arrayFormativeString = scanner.nextLine();
 
         Helperstuff helperClass = new Helperstuff();
-        int countofCommas = helperClass.getInstancesOfLetter(arrayString, ',');
+        int countofCommasSummative = helperClass.getInstancesOfLetter(arraySummativeString, ',') + 1;
+        int countofCommasFormative = helperClass.getInstancesOfLetter(arrayFormativeString, ',') + 1;
 
-        int[] awway = new int[countofCommas];
+        double[] summativeAwway = new double[countofCommasSummative];
+        double[] formativeAwway = new double[countofCommasFormative];
 
-        WhatIf test = new WhatIf(courseName, awway);
+        for (int i = 0; i<countofCommasSummative; i++) {
+            summativeAwway[i] = Double.parseDouble(arraySummativeString.substring(0, arraySummativeString.indexOf(",")));
+            arraySummativeString = arraySummativeString.substring(arraySummativeString.indexOf(","), arraySummativeString.length());
+        }
+
+        for (int i = 0; i<countofCommasFormative; i++) {
+            formativeAwway[i] = Double.parseDouble(arrayFormativeString.substring(0, arrayFormativeString.indexOf(",")));
+            arrayFormativeString = arrayFormativeString.substring(arrayFormativeString.indexOf(","), arrayFormativeString.length());
+        }
+
+        WhatIf test = new WhatIf(courseName, summativeAwway, formativeAwway);
+        test.calcAverage();
+        System.out.println(test);
 
     }
 }
